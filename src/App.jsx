81e31e126d29cf6c25298a6c6f5fd11a57a2a3a1
Chokseys2022 <choksey2022@gmail.com>
2024-03-12 16:52:06 -0700

    form elements added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 
-function reducer(state, action) {}
+const ACTIONS = {
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle_todo",
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE.TODO:
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
 
 export default function App() {
   const [todos, dispatch] = useReducer(reducer, []);
-}
+  const [name, setName] = useState("");
 
-return (
-  <>
-    <form>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-    </form>
-  </>
-);
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
+  }
+
+  console.log(todos);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      {todos.map((todo) => {
+        <Todo key={todo.id} todo={todo} />;
+      })}
+    </>
+  );
+}
