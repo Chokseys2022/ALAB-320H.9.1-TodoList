0dce693bfde481b80cd8db3beb240a2608c47d4d
Chokseys2022 <choksey2022@gmail.com>
2024-03-12 21:26:50 -0700

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,68 @@
+// import React, { useReducer, useState } from "react";
+// import Todo from "./Todo.jsx";
+
+// export const ACTIONS = {
+//   ADD_TODO: "add-todo",
+//   TOGGLE_TODO: "toggle_todo",
+//   DELETE_TODO: "delete_todo",
+//   EDIT_TODO: "edit_todo"
+// };
+
+// function reducer(todos, action) {
+//   switch (action.type) {
+//     case ACTIONS.ADD_TODO:
+//       return [...todos, newTodo(action.payload.name)];
+//     case ACTIONS.TOGGLE_TODO:
+//       return todos.map((todo) => {
+//         if (todo.id === action.payload.id) {
+//           return { ...todo, complete: !todo.complete };
+//         }
+//         return todo;
+//       });
+//     case ACTIONS.DELETE_TODO:
+//       return todos.filter(todo=> todo.id !== action.payload.id);
+
+//     default:
+//       return todos;
+//   }
+// }
+
+// function newTodo(name) {
+//   return { id: Date.now(), name: name, complete: false };
+// }
+
+// export default function App() {
+//   const [todos, dispatch] = useReducer(reducer, []);
+//   const [name, setName] = useState("");
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+//     setName("");
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//       </form>
+//       {todos.map((todo) => {
+//         return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
+//       })}
+//     </>
+//   );
+// }
+
+//////////////////////////////////////
 import React, { useReducer, useState } from "react";
 import Todo from "./Todo.jsx";
+import "./App.css"; 
+
+
 
 export const ACTIONS = {
   ADD_TODO: "add-todo",
@@ -10,7 +73,7 @@ export const ACTIONS = {
 function reducer(todos, action) {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.name)];
+      return [newTodo(action.payload.name), ...todos];
     case ACTIONS.TOGGLE_TODO:
       return todos.map((todo) => {
         if (todo.id === action.payload.id) {
@@ -19,8 +82,7 @@ function reducer(todos, action) {
         return todo;
       });
     case ACTIONS.DELETE_TODO:
-      return todos.filter(todo=> todo.id !== action.payload.id);
-
+      return todos.filter(todo => todo.id !== action.payload.id);
     default:
       return todos;
   }
@@ -41,17 +103,27 @@ export default function App() {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </form>
-      {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
-      })}
-    </>
+    <div className="background">
+      <div className="taskcontainer">
+        <h1 className="heading">Todo List</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="listItem"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter a new task..."
+          />
+          <button className="listItem" type="submit">Add Task</button>
+        </form>
+        {todos.length === 0 && (
+          <p className="noItem">No tasks available. Add some tasks!</p>
+        )}
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+        ))}
+      </div>
+    </div>
   );
 }
+
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,12 +1,46 @@
-import React from "react";
-import {ACTIONS} from './App'
+// import React from "react";
+// import {ACTIONS} from './App'
 
-export default function Todo({ todo, dispatch }) { // Added dispatch to props
-  return ( // Moved return here
-    <div>
-      <span style={{ color: todo.complete ? "#AAA" : "#000" }}>{todo.name}</span>
-      <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
+// export default function Todo({ todo, dispatch }) { // Added dispatch to props
+//   return ( // Moved return here
+//     <div>
+//       <span style={{ color: todo.complete ? "#AAA" : "#000" }}>{todo.name}</span>
+//       <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
+//       <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
+//     </div>
+//   );
+// }
+
+///////////////////////////////////////////////
+import React from "react";
+import { ACTIONS } from "./App";
+import "./App.css"; 
+
+
+
+export default function Todo({ todo, dispatch }) {
+  return (
+    <div className="todoItem">
+      <span className="todoName" style={{ color: todo.complete ? "#AAA" : "#000" }}>
+        {todo.name}
+      </span>
+      <div className="todoButtons">
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() =>
+            dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })
+          }
+        />
+        <button
+          onClick={() =>
+            dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })
+          }
+          disabled={!todo.complete}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
