13d28d985148ab0f2dd8478c5ffcf47bc476316f
Chokseys2022 <choksey2022@gmail.com>
2024-03-12 17:15:13 -0700

    button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useReducer, useState } from "react";
+import Todo from "./Todo.jsx";
 
 const ACTIONS = {
   ADD_TODO: "add-todo",
@@ -9,7 +10,7 @@ function reducer(todos, action) {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)];
-    case ACTIONS.TOGGLE.TODO:
+    case ACTIONS.TOGGLE_TODO:
   }
 }
 
@@ -24,10 +25,8 @@ export default function App() {
   function handleSubmit(e) {
     e.preventDefault();
     dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
-    setName("");
+    setName('');
   }
-
-  console.log(todos);
 
   return (
     <>
@@ -39,7 +38,7 @@ export default function App() {
         />
       </form>
       {todos.map((todo) => {
-        <Todo key={todo.id} todo={todo} />;
+        return <Todo key={todo.id} todo={todo} />;
       })}
     </>
   );
